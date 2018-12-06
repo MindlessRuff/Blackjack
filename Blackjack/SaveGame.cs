a98d817fb8eff42db7bc8b1c78f881aa00bae7ff
Brandon Veltre <bveltre@sdsu.edu>
2018-12-06 13:54:12 -0800

    Editied SaveGame

--- Blackjack/SaveGame.cs
+++ Blackjack/SaveGame.cs
@@ -1,23 +1,36 @@
-﻿using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Blackjack
 {
     class SaveGame
     {
         string json { get; set; }
+        string path { get; set; } 
         public SaveGame()
         {
-
+            path = @"Saves/SaveGame.txt";
         }
 
         public SaveGame(object save)
         {
             json = JsonConvert.SerializeObject(save, Formatting.Indented);
+        }
+
+        public void SaveObject()
+        {
+            if (!File.Exists(path))
+            {
+                using (FileStream fs = File.Create(path))
+                {
+                
+                }
+            }
         }
     }
 }
