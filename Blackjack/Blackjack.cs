968c17e58a773d4eb364ebacf1c5f0ec486dc115
MatthewNickels <matthewn91@hotmail.com>
2018-12-06 11:58:19 -0800

    Moar deletes

--- Blackjack/Blackjack.cs
+++ Blackjack/Blackjack.cs
@@ -42,22 +42,6 @@ namespace Blackjack
             // Print the game state to the Debug Console
             System.Diagnostics.Debug.Write(this.ToString());
         }    
-        /*
-            playerHand.Add(newDeck.Deal_Card());
-            dealerHand.Add(newDeck.Deal_Card());
-            playerHand.Add(newDeck.Deal_Card());
-            dealerHand.Add(newDeck.Deal_Card());
-
-            // Add their values
-            playerHandValue += Card_Value(playerHand[0]);
-            playerHandValue += Card_Value(playerHand[1]);
-            dealerHandValue += Card_Value(dealerHand[0]);
-            dealerHandValue += Card_Value(dealerHand[1]);
-            */
-            
-        
-
-
         /// <summary>
         /// 
         /// </summary>
