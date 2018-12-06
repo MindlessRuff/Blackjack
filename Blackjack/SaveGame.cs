94e5674403f432f0a9d0bdccc47725b0ee173f96
Brandon Veltre <bveltre@sdsu.edu>
2018-12-06 13:18:28 -0800

    Added SaveGame.cd

---
+++ Blackjack/SaveGame.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack
+{
+    class SaveGame
+    {
+        string json { get; set; }
+        public SaveGame()
+        {
+
+        }
+
+        public SaveGame(object save)
+        {
+            json = JsonConvert.SerializeObject(save, Formatting.Indented);
+        }
+    }
+}
